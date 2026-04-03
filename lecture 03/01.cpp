f7538fd5acdfa0992336e5a3f0cab1405a95637f
SHUBHAM <shubhamkumar29141@gmail.com>
2026-04-03 12:37:38 +0530

    Create 01.cpp

---
+++ lecture 03/01.cpp
@@ -0,0 +1,16 @@
+// if-else odd even
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter Number";
+    cin >> n;
+
+    if(n % 2 == 0){
+        cout << "even\n";
+    } else{
+        cout << "odd\n";
+    }
+    return 0;
+}
