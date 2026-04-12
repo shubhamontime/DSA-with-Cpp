c2735d381bf4b7245d0a3937d848b2af6e589192
SHUBHAM <shubhamkumar29141@gmail.com>
2026-04-12 17:53:27 +0530

    Create 12.cpp

---
+++ lecture 04/12.cpp
@@ -0,0 +1,27 @@
+//1111
+// 222
+//  33
+//   4
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n = 4;
+
+    for(int i = 0; i < n; i++){
+
+        // spaces
+        for(int j = 0; j < i; j++){
+            cout << " ";
+        }
+
+        // numbers (FIXED)
+        for(int j = 0; j < n - i; j++){
+            cout << i + 1;
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
