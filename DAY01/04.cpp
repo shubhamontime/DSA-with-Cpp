ffde61595453aabbe8bc46f2aae340bfbe87899b
SHUBHAM KUMAR <shubhamkumar29141@gmail.com>
2026-06-27 23:51:53 +0530

    Create 04.cpp

---
+++ DAY01/04.cpp
@@ -0,0 +1,18 @@
+//11111
+//11111
+//11111
+//11111
+//11111
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    for(int row=1;row<=5;row++){
+        for(int col=1; col<=5; col++){
+        cout<<"1 ";    
+        }
+      cout<<endl;
+    }
+    return 0;
+}
