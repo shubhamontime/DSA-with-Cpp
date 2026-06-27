39fd4616b6edd53e9d9f9d23a1b6cd972522b4e6
SHUBHAM KUMAR <shubhamkumar29141@gmail.com>
2026-06-27 23:52:18 +0530

    Add program to print square matrix of numbers
    This program takes an integer input and prints a square matrix of numbers from 1 to n.

---
+++ DAY01/05.cpp
@@ -0,0 +1,18 @@
+//1 2 3 4 5 
+//1 2 3 4 5 
+//1 2 3 4 5 
+//1 2 3 4 5
+//1 2 3 4 5 
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+   for(int row=1;row<=n;row++){
+    for(int col=1;col<=n;col++){
+        cout<<col<<" ";
+    }
+    cout<<endl;
+   }
+   return 0;
+}
