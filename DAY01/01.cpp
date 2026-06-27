ccabee8d799438cd2a7985ad7b662ceee9207a4c
SHUBHAM KUMAR <shubhamkumar29141@gmail.com>
2026-06-27 23:50:30 +0530

    Create 01.cpp

---
+++ DAY01/01.cpp
@@ -0,0 +1,13 @@
+//use this code in write any table 1 to 10 times
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cin>>num;
+
+    for(int i=2;i<=10;i++){
+        cout << num * i <<endl;
+    }
+    return 0;
+}
